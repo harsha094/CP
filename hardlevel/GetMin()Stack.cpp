4d2e52fc1b0801601efdc0a340ce49c0f18ec420
Harsh Raghuwanshi <56026307+harsha094@users.noreply.github.com>
2022-06-17 04:43:19 +0530

    Added initial cpp files

---
+++ hardlevel/GetMin()Stack.cpp
@@ -0,0 +1,74 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+class SpecialStack {
+    // Define the data members.
+    stack<int> s;
+    int mini;
+
+    /*----------------- Public Functions of SpecialStack -----------------*/
+    public:
+        
+    void push(int data) {
+        // for first element
+        if(s.empty()){
+            s.push(data);
+            mini = data;
+        }
+        else{
+            if(data < mini){
+                s.push(2*data - mini);
+                mini = data;
+            }
+            else{
+                s.push(data);
+            }
+        }
+    }
+
+    int pop() {
+        // check underflow
+        if(s.empty()){
+            return -1;
+        }
+        int curr = s.top();
+        s.pop();
+        if(curr > mini){
+            return curr;
+        }
+        else{
+           int prevMin = mini;
+           int val = 2*mini - curr;
+           mini = val;
+           return prevMin;
+        }
+    }
+
+    int top() {
+        // Implement the top() function.
+        if(s.empty()){
+            return -1;
+        }
+        int curr = s.top();
+        if(curr < mini){
+            return mini;
+        }
+        else
+            return curr;
+    }
+
+    bool isEmpty() {
+        // Implement the isEmpty() function.
+        return s.empty();
+    }
+
+    int getMin() {
+        // Implement the getMin() function.
+        if(s.empty()){
+            return -1;
+        }
+        
+        return mini;
+    }  
+};
