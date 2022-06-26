c915f550b826dc057da76c503694a65f6880538b
Harsh Raghuwanshi <56026307+harsha094@users.noreply.github.com>
2022-06-27 03:08:02 +0530

    self practice problems

---
+++ practice/runningSum-array.cpp
@@ -0,0 +1,18 @@
+#include<vector>
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        int sum=0;
+        vector<int>ans;
+        for(int i=0;i<nums.size();i++){
+            sum += nums[i];
+            ans.push_back(sum);
+        }
+        return ans;
+        
+        //for(int i=1;i<nums.size();i++)
+          //  nums[i]+= nums[i-1];
+        //return nums;
+        
+    }
+};
