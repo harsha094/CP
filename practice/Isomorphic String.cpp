6a077ac7a81a01f3646babf110d0f01e3ac952f3
Harsh Raghuwanshi <56026307+harsha094@users.noreply.github.com>
2022-07-03 23:39:27 +0530

    String Practice question
    Question link :-  https://leetcode.com/problems/isomorphic-strings/

---
+++ practice/Isomorphic String.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        int n = s.length();
+        map<char, int> m1, m2;
+        for (int k=0; k<n; k++){
+            if (m1[s[k]]==0 && m2[t[k]]==0){
+                m1[s[k]] = k+1;
+                m2[t[k]] = k+1;
+            }
+            if (m1[s[k]]!=m2[t[k]]){
+                return false;
+            }
+        }
+        return true;
+    }
+};
