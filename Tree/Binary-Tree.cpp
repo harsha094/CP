6d757153bc7579085b323110e96b23ee16b281e6
Harsh Raghuwanshi <56026307+harsha094@users.noreply.github.com>
2022-07-06 17:38:43 +0530

    Updated Binary Tree

--- Tree/Binary-Tree.cpp
+++ Tree/Binary-Tree.cpp
@@ -59,6 +59,39 @@ void levelOrderTraversal(node* root){
     
 }
 
+void inorder(node* root){
+    //base case
+    if(root == NULL){
+        return;
+    }
+
+    inorder(root->left);
+    cout<< root-> data << " ";
+    inorder(root->right);
+}
+
+void preorder(node* root){
+    //base case
+    if(root == NULL){
+        return;
+    }
+
+    cout<< root-> data << " ";
+    preorder(root->left);
+    preorder(root->right);
+}
+
+void postorder(node* root){
+    //base case
+    if(root == NULL){
+        return;
+    }
+
+    postorder(root->left);
+    postorder(root->right);
+    cout<< root-> data << " ";
+}
+
 int main(){
     node* root = NULL;
     //creating a tree
@@ -68,5 +101,50 @@ int main(){
     cout<< "Printing the level order traversal output" << endl;
     levelOrderTraversal(root);
 
+    cout<< " Inorder traversal is ";
+    inorder(root);
+
+    cout<< "\n Preorder traversal is ";
+    preorder(root);
+
+    cout<< "\n Postorder traversal is ";
+    postorder(root);
+
     return 0;
 }
+
+/* Output - 
+Enter the data: 
+1 3 7 -1 -1 11 -1 -1 5 17 -1 -1 -1
+Enter data for inserting in left of 1
+Enter the data:
+Enter data for inserting in left of 3
+Enter the data:
+Enter data for inserting in left of 7
+Enter the data:
+Enter data for inserting in right of 7
+Enter the data:
+Enter data for inserting in right of 3
+Enter the data:
+Enter data for inserting in left of 11
+Enter the data: 
+Enter data for inserting in right of 11
+Enter the data:
+Enter data for inserting in right of 1
+Enter the data:
+Enter data for inserting in left of 5
+Enter the data:
+Enter data for inserting in left of 17
+Enter the data:
+Enter data for inserting in right of 17
+Enter the data:
+Enter data for inserting in right of 5
+Enter the data:
+Printing the level order traversal output
+1
+3 5
+7 11 17
+ Inorder traversal is 7 3 11 1 17 5
+ Preorder traversal is 1 3 7 11 5 17
+ Postorder traversal is 7 11 3 17 5 1
+*/
